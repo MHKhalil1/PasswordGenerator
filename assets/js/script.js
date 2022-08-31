aa8dc3c6a6c99593c5fe7b6d0ef13b8e9b5b2675
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-08-31 16:51:18 -0400

    Randomized Functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+function randomInt (min,max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor (min*(1 - rand) + rand*max)
+}
+function getRandomItem(list) {
+  return list[randomInt(list.length)]
+}
+function generatePassword() {
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
